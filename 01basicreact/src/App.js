6d043fe029d6f12079d96c21903de7209fbff3c7
Rahul Vishwakarma <93070303+vishrahul2002@users.noreply.github.com>
2024-02-10 08:37:16 +0000

    learning to create a new components and how to use it.

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,6 +1,11 @@
+import Chai from "./chai"
+
 function App() {
   return (
+    <>
+    <Chai />
     <h1>Hello world</h1>
+    </>
   );
 }
 
---
+++ 01basicreact/src/chai.js
@@ -0,0 +1,8 @@
+function Chai() {
+    return(
+        <h2>Chai in React</h2>
+    )
+
+}
+
+export default Chai
